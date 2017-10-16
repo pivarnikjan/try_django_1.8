88fbdbb0d614dd4614bae222783f3998de4e2fb7
jp002f <jp002f@intl.att.com>
2017-10-16 11:14:11 +0200

    [ADD] added custom forms

--- trydjango18/newsletter/forms.py
+++ trydjango18/newsletter/forms.py
@@ -3,6 +3,20 @@ from django import forms
 from .models import SignUp
 
 
+class ContactForm(forms.Form):
+    full_name = forms.CharField(required=False)
+    email = forms.EmailField()
+    message = forms.CharField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
+
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
@@ -15,3 +29,7 @@ class SignUpForm(forms.ModelForm):
         if not extension == "edu":
             raise forms.ValidationError("Please use a valid .EDU email address")
         return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        return full_name
--- trydjango18/newsletter/views.py
+++ trydjango18/newsletter/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from .forms import ContactForm, SignUpForm
 
 
 def home(request):
-    return render(request, "home.html", {})
+    title = "Welcome"
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "form": form,
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get('full_name')
+
+        if not full_name:
+            instance.full_name = "Justin Bieber"
+        instance.save()
+        context = {
+            "title": "Thank you"
+        }
+
+    return render(request, "home.html", context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        for key, value in form.cleaned_data.items():
+            print(key, value)
+
+    context = {
+        "form": form,
+    }
+    return render(request, "forms.html", context)
